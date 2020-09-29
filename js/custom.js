adfb139f11d8a0ed0cc18ff0510c63401c39cff0
TheoAcker12 <theo.o.acker-1@ou.edu>
2020-09-29 10:18:44 -0500

    fixed close popup

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ function handleStepEnter(response) {
     features.get(response.element.getAttribute("feature")).openPopup();
 }
 function handleStepExit(response) {
-    features.get(response.element.getAttribute("feature")).openPopup();
+    features.get(response.element.getAttribute("feature")).closePopup();
 }
 function init() {
     scroller
